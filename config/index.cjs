f7e0a009f2cbc33786806131fd0e15483c1df658
shawpan <shawpan@yeah.net>
2024-04-20 21:45:25 +0800

    Update index.cjs
    test

--- config/index.cjs
+++ config/index.cjs
@@ -21,11 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'olGdV6Wf7cpiKsLhV-oRlANu3F7M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'D9rmrlRxAt7FD1WPnENkndvrxFYZYBxmMzjgyMTM0DU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-14',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '	pqHNvtiBPtv5aQohrHagIoO7Rg1owUPYlRxCuDi_Y8I',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'olGdV6VLuTgi-LLVzJVsKexMA9N8',
     }
   ],
 
